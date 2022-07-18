e9b3b1eb72bf9664c042b1e351bc34116353e9da
Thomas <thomasgleizes34@gmail.com>
2022-07-18 12:25:27 +0200

    replace create-hash by node:crypto

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,18 +1,14 @@
-import createHash from "create-hash";
+import crypto from "node:crypto";
 import jwt from "jsonwebtoken";
 
-export function sha256(stringToHash: string): string {
-  const hash = createHash("sha512");
+export function sha512(stringToHash: string): string {
+  const hash = crypto.createHash("sha512");
 
   hash.update(process.env.SECRET_SEED + stringToHash);
   const hashedString: string = hash.digest().toString("hex");
   hash.end();
 
   return hashedString;
-}
-
-export function compareHash(str: string, encrypted: string): boolean {
-  return sha256(str) === encrypted;
 }
 
 export function createJwt(payload: any): string {
